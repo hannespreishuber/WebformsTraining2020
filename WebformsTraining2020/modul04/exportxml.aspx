f93dea683d0d56517af12ae43b813a86bae74736
Hannes Preishuber <hannesp@ppedv.de>
2020-10-06 16:30:13 +0200

    Export XML und Excel

---
+++ WebformsTraining2020/modul04/exportxml.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="exportxml.aspx.cs" Inherits="WebformsTraining2020.modul04.exportxml" %>
+
+   <table>
+            <%foreach (var item in MyListe)
+                {%>
+            <tr>
+                <td><%=item.Key %></td>
+                <td><%=item.Value %></td>
+            </tr>
+
+            <%} %>
+        </table>
--- WebformsTraining2020/modul04/exportxml.aspx.cs
+++ WebformsTraining2020/modul04/exportxml.aspx.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Services.Description;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace WebformsTraining2020.modul04
 {
-    public partial class exportexcel : System.Web.UI.Page
+    public partial class exportxml : System.Web.UI.Page
     {
-        public Dictionary<string,DateTime> MyListe { get; set; }=new Dictionary<string, DateTime>();
+        public Dictionary<string, DateTime> MyListe { get; set; } = new Dictionary<string, DateTime>();
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            MyListe.Add("Franz",DateTime.Now.AddDays(232));
+            MyListe.Add("Franz", DateTime.Now.AddDays(232));
             MyListe.Add("Maria", DateTime.Now.AddDays(-32));
             MyListe.Add("Sepp", DateTime.Now.AddDays(34));
             MyListe.Add("Diana", DateTime.Now.AddDays(2));
-            Response.ContentType = "application/vnd.ms-excel";
-            Response.Headers.Add("Content-Disposition","filename=Daten.xls");
+            Response.ContentType = "application/xml";
+
         }
     }
 }
