3d035d490fd755a0f10c937644cdcc0d8656e732
Hannes Preishuber <hannesp@ppedv.de>
2020-10-06 16:17:22 +0200

    Excel export statt Browser als UI

--- WebformsTraining2020/modul04/exportexcel.aspx.cs
+++ WebformsTraining2020/modul04/exportexcel.aspx.cs
@@ -10,14 +10,15 @@ namespace WebformsTraining2020.modul04
 {
     public partial class exportexcel : System.Web.UI.Page
     {
-        public Dictionary<string,DateTime> MyListe { get; set; }
+        public Dictionary<string,DateTime> MyListe { get; set; }=new Dictionary<string, DateTime>();
         protected void Page_Load(object sender, EventArgs e)
         {
+
             MyListe.Add("Franz",DateTime.Now.AddDays(232));
             MyListe.Add("Maria", DateTime.Now.AddDays(-32));
             MyListe.Add("Sepp", DateTime.Now.AddDays(34));
             MyListe.Add("Diana", DateTime.Now.AddDays(2));
-
+            Response.ContentType = "application/vnd.ms-excel";
         }
     }
 }
