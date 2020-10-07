59f86b47eb938f590ff90046fd799121daeaa516
Hannes Preishuber <hannesp@ppedv.de>
2020-10-07 12:04:09 +0200

    Cookies Basic

---
+++ WebformsTraining2020/modul05/WebForm8.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsTraining2020.modul05
+{
+    public partial class WebForm8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            var wert = Request.Cookies["hannes"];
+            if (wert != null)
+            {
+                if (!IsPostBack)
+                {
+                    TextBox1.Text = wert.Value;
+
+                }
+
+
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var cookie = new HttpCookie("hannes");
+            cookie.Value = TextBox1.Text;
+            cookie.Expires = DateTime.Now.AddDays(30);
+            // cookie.Domain = ".ppedv.de";
+            Response.Cookies.Set(cookie);
+            //Response.AppendCookie(cookie);
+        }
+    }
+}
