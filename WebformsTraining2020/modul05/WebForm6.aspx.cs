e1ff65d33bf9c9508e9a4de32db2f0e38baa8727
Hannes Preishuber <hannesp@ppedv.de>
2020-10-07 11:39:57 +0200

    Session Variablen

---
+++ WebformsTraining2020/modul05/WebForm6.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsTraining2020.modul05
+{
+    public partial class WebForm6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["eingabe"] = TextBox1.Text;
+            Response.Redirect("webform7");
+        }
+    }
+}
---
+++ WebformsTraining2020/modul05/WebForm7.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm7.aspx.cs" Inherits="WebformsTraining2020.modul05.WebForm7" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <h1>Seite 2</h1>
+    <form id="form1" runat="server">
+        <div>
+            <%=Session["eingabe"] %>
+        </div>
+    </form>
+</body>
+</html>
