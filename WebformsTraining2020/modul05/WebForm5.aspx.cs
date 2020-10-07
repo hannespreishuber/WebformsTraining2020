4c4205709d4a6fefb5a231ee387fd1fb9de7f6a3
Hannes Preishuber <hannesp@ppedv.de>
2020-10-07 11:32:21 +0200

    BUffer mit Application Variable Orte Liste

---
+++ WebformsTraining2020/modul05/WebForm5.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsTraining2020.modul05
+{
+    public partial class WebForm5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var x = Application["orte"];
+            if (x==null)
+            {
+                Application["orte"] = File.ReadAllLines(Server.MapPath(@"~\app_data\plzort.csv"));
+            }
+        }
+    }
+}
