832b72eac6f1ccc806153d73923ab361d0158e08
Hannes Preishuber <hannesp@ppedv.de>
2020-10-08 13:49:37 +0200

    Model Binding Liste

---
+++ WebformsTraining2020/modul09/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/modul07/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebformsTraining2020.modul09.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="list-group">
+        <div class="list-group-item list-group-item-primary">Northwind Kunden</div>
+        <asp:Repeater ID="Repeater1" runat="server"
+            ItemType="WebformsTraining2020.modul08.Kunden"
+            SelectMethod="Repeater1_GetData"
+             >
+            <ItemTemplate>
+                <div class="list-group-item ">
+                    <div class="row">
+                        <div class="col-2"><%#Item.ID%></div>
+                        <div class="col-8"><%# Item.Firmenname %> </div>
+                        <div class="col-1">
+                            <a href="WebForm3/<%#Item.ID%>">
+                                <span class="oi oi-brush"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+</asp:Content>
---
+++ WebformsTraining2020/modul09/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebformsTraining2020.modul08;
+
+namespace WebformsTraining2020.modul09
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IEnumerable<WebformsTraining2020.modul08.Kunden> Repeater1_GetData()
+        {
+            var liste = new List<Kunden>();
+            using (var con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["northwindConnectionString1"].ConnectionString))
+            {
+                var sql = "select * from customers ";
+                var cmd = new SqlCommand(sql, con);
+                con.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    liste.Add(new Kunden()
+                    {
+                        ID = reader["CustomerID"].ToString(),
+                        Firmenname = reader["CompanyName"].ToString()
+                    });
+                }
+
+            }
+            return liste.ToList();
+        }
+    }
+}
