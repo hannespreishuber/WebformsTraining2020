6c1d2a65c74cc684fac1a8f4b8fa916f98a66fd2
Hannes Preishuber <hannesp@ppedv.de>
2020-10-08 16:30:23 +0200

    Web.Config Error Messages anzeigen

---
+++ WebformsTraining2020/WebForm1.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/modul07/Site1.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebformsTraining2020.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click"/>
+</asp:Content>
---
+++ WebformsTraining2020/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebformsTraining2020
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            int x = 1 / i;
+
+        }
+    }
+}
