bad852f3b821baacd2aa0adc410024834406644e
Hannes Preishuber <hannesp@ppedv.de>
2020-10-09 13:18:12 +0200

    ASMX Service

---
+++ WebformsTraining2020/modul15/WebService1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebService1.asmx.cs" Class="WebformsTraining2020.modul15.WebService1" %>
---
+++ WebformsTraining2020/modul15/WebService1.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using WebformsTraining2020.modul09;
+
+namespace WebformsTraining2020.modul15
+{
+    /// <summary>
+    /// Zusammenfassungsbeschreibung für WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Wenn der Aufruf dieses Webdiensts aus einem Skript zulässig sein soll, heben Sie mithilfe von ASP.NET AJAX die Kommentarmarkierung für die folgende Zeile auf. 
+  [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        //[WebMethod]
+        //public List<Customers>GetCustomers()
+        //{
+
+        //    var db = new Model1();
+        //    //crasht weil Serialisierung Problem LacyLoading
+            
+        //    return db.Customers.ToList();
+        //}
+    }
+}
